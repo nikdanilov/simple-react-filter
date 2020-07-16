40dcb59a40c27841a9dc746eb7265c036a6f5f79
Nik Danilov <dev.nik.danilov@gmail.com>
2020-07-16 18:56:29 -0400

    Adds tailwindcss & updates filter component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./App.css"
+import "./App.css";
 
 const languages = [
   "Python",
@@ -18,35 +18,44 @@ function App() {
 
   useEffect(() => {
     const results = languages.filter((lang) =>
-      lang.toLowerCase().includes(search.toLocaleLowerCase())
+      lang.toLowerCase() === search.toLowerCase()
     );
     setSearchResults(results);
   }, [search]);
 
   return (
     <div className="App">
-      <div
-        style={{
-          display: "block",
-          border: "2px solid green",
-          width: "20%",
-          textAlign: "center",
-          margin: "0 auto",
-          marginTop: "2rem",
-        }}
-      >
-        <h1>React Filter</h1>
-        <input
-          type="text"
-          placeholder="Search"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          />
-        <ul style={{margin:"1rem"}}>
-          {searchResults.map((item) => (
-            <li>{item}</li>
-          ))}
-        </ul>
+      <div className="min-h-screen bg-gradient sm:p-3.5 sm:flex">
+        <div className="sm:flex-1 font-sans bg-sepia-500 text-sepia-900 sm:rounded leading-normal relative overflow-x-hidden">
+          <div className="sm:flex-1 sm:flex flex-col pt-6 md:max-w-6xl mx-6 lg:mx-auto">
+            <main className="flex-1">
+            <div className="mt-16 animation animation-fadeInUp animation-delay-2">
+              <h3 className="inline-block rounded-lg m-0 text-sepia-900 bg-white text-4xl px-3 font-main" style={{transform: "rotate(-1deg)", boxShadow: "0 0 4px var(--white)"}}>Snippets</h3>
+              <p className="mt-4 article font-main">A collection of short snippets I used to solve common problems.</p>
+            </div>
+              <div className="mt-2 animation animation-fadeInUp animation-delay-2 border-2 border-brand rounded-lg pl-2 py-1 flex items-center">
+                <span className="font-medium text-gradient">Filter:</span>
+                <div className="flex items-center overflow-x-auto overflow-y-hidden">
+                  {languages.map((item) => (
+                    <span
+                      id={item}
+                      key={item}
+                      className={`${search === item ? 'bg-gradient' : ''} bg-sepia-300 inline-flex items-center ml-2 px-3 py-1 rounded-full text-sm font-medium leading-5 text-white cursor-pointer`}
+                      onClick={(e) => setSearch(e.target.id)}
+                    >
+                      {item}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <ul style={{ margin: "1rem" }}>
+                {searchResults.map((item) => (
+                  <li key={item}>{item}</li>
+                ))}
+              </ul>
+            </main>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        sepia: {
+          100: "rgba(60, 55, 30, 0.1)",
+          300: "rgba(60, 55, 30, 0.3)",
+          500: "#f5f5f0",
+          600: "rgba(60, 55, 30, 0.6)",
+          900: "rgba(60, 55, 30)",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
